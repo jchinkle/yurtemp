dc518db770a9a8789ce39a39240d6c4c61c89486
Jeremy Hinkle <jchinkle@gmail.com>
2013-12-15 20:38:19 -0600

    Added index to temperatures for created at date

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131215234855) do
+ActiveRecord::Schema.define(version: 20131216023729) do
 
   create_table "temperatures", force: true do |t|
     t.float    "degrees"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "temperatures", ["created_at"], name: "index_temperatures_on_created_at"
 
 end
